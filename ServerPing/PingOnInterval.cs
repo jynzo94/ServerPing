71ae0ab18a58e9ef4801bac76fd154f32821ca76
Alexander Kolarov <alexander.m.kolarov@gmail.com>
2022-01-17 15:55:33 +0200

    Initial commit

---
+++ ServerPing/PingOnInterval.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPing
+{
+    public class PingOnInterval
+    {
+        private string _ip;
+
+        public PingOnInterval(string ip)
+        {
+            _ip = ip;
+            
+            PingServer();
+
+            Start().Wait();
+        }
+
+        public async Task Start()
+        {
+            var timespan = TimeSpan.FromMinutes(10);
+            await SetInterval(PingServer, timespan);
+        }
+
+        private void PingServer()
+        {
+            Ping ping = new Ping();
+
+            var result = ping.Send(_ip);
+
+            if (result.Status == IPStatus.Success)
+            {
+                Console.WriteLine($"Ping successed");
+            }
+            else
+            {
+                Console.WriteLine($"Ping failed");
+            }
+
+
+        }
+
+        private async Task SetInterval(Action action, TimeSpan timeout)
+        {
+            await Task.Delay(timeout).ConfigureAwait(false);
+
+            action();
+
+            await SetInterval(action, timeout);
+        }
+
+    }
+
+
+}
---
+++ ServerPing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace ServerPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ping on interval started");
+            var pingOnInterval = new PingOnInterval("45.58.142.21");
+        }
+
+
+    }
+}
